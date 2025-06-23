26ba332a6eecf4f65d45ededc206b6442d3fcfcd
Saya-ok <cazorlaigor@gmail.com>
2025-06-23 01:08:23 +0100

    Initial commit: DOM Manipulation

---
+++ Day1.js
@@ -0,0 +1,34 @@
+// Welcome to Day 1
+// JUNE 16 2025
+// Today we are learning about variables,
+
+let message;
+
+message = "Hello";
+
+// Im Moving on trying the quiz now. I will right my reflections
+//
+// 3.
+//null means “intentional absence of any object value.” It’s like saying: “This variable has no value, and that’s on purpose.”
+//
+//undefined means “a variable has been declared but not assigned any value yet.” It’s the default state of a variable that exists but hasn’t been given a value.
+//
+//  undefined = default as nothing has been assigned yet. null = nothing but its meant to be that way.
+//
+// 4.
+// Numbers cannot be at the start of a variable name
+//
+// 5.
+//  NaN is Not a number but in JS IT IS A NUMBER TECHNICALLY
+// NaN is actually a special numeric value that means “invalid number” or “not a valid number.”
+//  null is an OBJECT
+//
+// Focus Areas Before Day 2:
+// Hoisting vs initialization — especially differences with var and let.
+// Understanding scope — block vs function.
+// JavaScript quirks like typeof null and typeof NaN.
+// Avoiding accidental globals and the benefits of strict mode.
+//
+//
+//
+//
